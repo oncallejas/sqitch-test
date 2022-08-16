d33e8e26f51e2320e39687a6993b806e2f9c8771
oncallejas <oncallejas@stefanini.com>
2022-08-16 16:11:40 -0600

    Add users table

---
+++ revert/users.sql
@@ -0,0 +1,3 @@
+-- Revert flipr:users from oracle
+DROP TABLE flipr.users;
+-- XXX Add DDLs here.
---
+++ verify/users.sql
@@ -0,0 +1,5 @@
+-- Verify flipr:users on oracle
+SELECT nickname, password, timestamp
+  FROM flipr.users
+ WHERE 0 = 1;
+-- XXX Add verifications here.
